bbf0bad53b96492c1176f99aeb588c95801eaad8
Yehang Zhu <zhuyehang001@live.com>
2017-12-28 21:53:21 +0800

    <chore> add folder git to store .gitconfig

--- update.sh
+++ update.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+# tmux configuration
 cp ~/.tmux.conf tmux/.tmux.conf
+
+# vim configuration
 cp ~/.vimrc vim/.vimrc
 cp ~/.vim vim/ -rf
+
+# git configuration
+cp ~/.gitconfig git/.gitconfig
