e64237bce3cfb2d9d63d269d0e0fbed88716a7a1
Yehang Zhu <zhuyehang001@live.com>
2017-12-29 09:40:51 +0800

    <chore> add setup script to update dot files

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# tmux configuration
+cp tmux/.tmux.conf ~/.tmux.conf
+
+# vim configuration
+cp vim/.vimrc ~/.vimrc
+cp vim/.vim -rf ~/
+
+# git configuration
+cp git/.gitconfig ~/.gitconfig
